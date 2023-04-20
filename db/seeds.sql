91eed85cca425b8a46565146bfb6edb190109fa0
Addit <adam.garza33@hotmail.com>
2023-04-20 18:19:06 -0500

    inserting new records into my tables

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+-- insert new records in the tables
+
+INSERT INTO departments (departments_name)
+VALUES
+('Design'),
+('Marketing'),
+('Engineering (Tech)'),
+('Sales'),
+('Finance'),
+('Business Strategy'),
+('Legal');
+
+INSERT INTO roles (departments_name, job_title, salary)
+VALUES
+('UX Artist', 45000, 1),
+('', , ),
+(''),
+(''),
+(''),
+(''),
+(''),
+(''),
+('');
