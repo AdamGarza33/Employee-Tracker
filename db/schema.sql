cc4a46ccaee0af6d4b0c0fd90b7a758995a714fa
Addit <adam.garza33@hotmail.com>
2023-04-20 18:07:57 -0500

    finished contents for my tables

--- db/schema.sql
+++ db/schema.sql
@@ -14,9 +14,15 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     departments_id INTEGER NOT NULL,
-    CONSTRAINT 
+    CONSTRAINT fk_departments FOREIGN KEY (departments_id) REFRENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
-
+    id INTEGER AUTO_INCREMENT PRIMARY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INTEGER NULL,
+    manager_id INTEGER NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
