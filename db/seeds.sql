a2687bd3f51d8e0efb741726c1529bc2f1a87553
Addit <adam.garza33@hotmail.com>
2023-04-20 18:30:03 -0500

    inserting new records into roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,11 +13,11 @@ VALUES
 INSERT INTO roles (departments_name, job_title, salary)
 VALUES
 ('UX Artist', 45000, 1),
-('', , ),
-(''),
-(''),
-(''),
-(''),
-(''),
-(''),
-('');
+('Sales Associate', 25500, 4),
+('Corportate Attorney', 130000, 7),
+('Software Engineer', 80000, 3),
+('Junior Developer', 45000, 3),
+('Financial Planner', 90000, 5),
+('Business Strategist', 150000, 6),
+('Director of Marketing', 200000, 2);
+
