3457926b8df401fa94fab2fbe8b7be8526b98cc6
Addit <adam.garza33@hotmail.com>
2023-04-20 18:37:24 -0500

    inserted new records into employees table

--- db/seeds.sql
+++ db/seeds.sql
@@ -21,3 +21,12 @@ VALUES
 ('Business Strategist', 150000, 6),
 ('Director of Marketing', 200000, 2);
 
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Ileesa', 'Ramirez', 2, 2),
+('Margarita', 'Flores', 1, 1),
+('Rodolpho', 'Silva', 4, 3),
+('Marcus', 'Rashford', 3, 1),
+('Alejandro', 'Garnacho', 6, 3),
+('Mario', 'Gotze', 5, 1),
+('Erling', 'Haaland', 7, 1);
