ea141a15228974873ae8712cb0847e0c82d81b23
Addit <adam.garza33@hotmail.com>
2023-04-20 19:08:56 -0500

    added prompt questions and used switch statement

--- server.js
+++ server.js
@@ -25,4 +25,48 @@ db.connect(err => {
 });
 
 // Prompt functions
+function startPrompt() {
+    inquirer.prompt({
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Update An Employee Manager', 'Delete Department', 'Delete Role', 'Delete Employee'], 
 
+    }).then( answer => {
+        switch (answer.menu) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add A Department':
+                addDepartment();
+                break;
+            case 'Add A Role':
+                addRole();
+                break;
+            case 'Add An Employee':
+                addEmployee();
+                break;
+            case 'Update An Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Update An Employee Manager':
+                updateEmployeeManager();
+                break;
+            case 'Delete A Department':
+                deleteDepartment();
+                break;
+            case 'Delete A Role':
+                deleteRole();
+                break;
+            case 'Delete An Employee':
+                deleteEmployee();
+                break;
+        }
+    })
+ };
